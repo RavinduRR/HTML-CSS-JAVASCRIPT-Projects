62000bcdaf894d993351f195a4f2916ae2c4ebea
RavinduRR <rshan9740@gmail.com>
2025-01-10 22:14:11 +0530

    T21

---
+++ 21.Random Emojies/app.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('.emoji')
+
+const emoji = [
+    "😂",
+    "😊",
+    "😍",
+    "👌",
+    "🙌",
+    "😁",
+    "🥲",
+    "✍️",
+    "🔥",
+    "✌️",
+    "😒",
+    "😘",
+    "🎯",
+    "😊",
+];
+
+btn.addEventListener('mouseover', () => {
+    btn.innerHTML = emoji[Math.floor(Math.random() * emoji.length)];
+})
