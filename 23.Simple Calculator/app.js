94188138ef4670ec2de5b0a7308d58947c944790
RavinduRR <rshan9740@gmail.com>
2025-01-14 23:47:23 +0530

    T23

---
+++ 23.Simple Calculator/app.js
@@ -0,0 +1,6 @@
+document.getElementById('btn').addEventListener('click', function(){
+    let num1 = document.querySelector('.num1').value
+    let num2 = document.querySelector('.num2').value
+    let result = document.querySelector('.result').value
+    let oprator = document.querySelector('.select0p').value
+})
